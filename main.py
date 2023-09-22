1b4c049e69b493fdca9057d904db97dd674914e9
asmku607c2s28911 <144655078+asmku607c2s28911@users.noreply.github.com>
2023-09-22 14:20:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Amount must be greater than 0.")
-
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
-
-    def display_balance(self):
-        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
-
-
-# Create an instance of the BankAccount class
-my_account = BankAccount("123456789", "John Doe", 1000)
-
-# Test deposit and withdrawal functionality
-my_account.display_balance()
-my_account.deposit(500)
-my_account.withdraw(200)
-my_account.display_balance()
+# leap year
+"""
+year % 4 == 0
+year % 100 != 0
+year % 400 == 0
+"""
+def isleapyear(year):
+  if (year % 4 == 0 and year % 100   != 0) or year % 400 == 0:
+    return True
+  else:
+    return False
+year = 2012
+if isleapyear(year):
+  print('{}is a leap year.'.
+format(year))
+else:
+  print ('{} is not a lea pyear.'.
+format(year))
